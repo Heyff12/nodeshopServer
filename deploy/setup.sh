519453967962e1361344d39c9e89c52f36e8a72e
Heyff12 <645068564@qq.com>
2020-05-08 00:24:42 +0800

    fix json path

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,10 +3,12 @@
 # use admin;
 # db.createUser({ user: 'root', pwd: 'rootyaya12', roles: [ { role: "userAdminAnyDatabase", db: "admin" } ] });
 mongo <<EOF
+
 use dumall;
 db.createCollection("goods");
 db.createCollection("users");
+
 EOF
 
-mongoimport --db dumall --collection goods --file $WORKSPACE/dumall-goods.json
-mongoimport --db dumall --collection users --file $WORKSPACE/dumall-users.json
+mongoimport --db dumall --collection goods --file /usr/local/work/dumall-goods.json
+mongoimport --db dumall --collection users --file /usr/local/work/dumall-users.json
