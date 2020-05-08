99df1950c5ba556b5ed41e8823a59737921a2116
Heyff12 <645068564@qq.com>
2020-05-08 09:43:47 +0800

    fix kong

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,11 +3,9 @@
 # use admin;
 # db.createUser({ user: 'root', pwd: 'rootyaya12', roles: [ { role: "userAdminAnyDatabase", db: "admin" } ] });
 mongo <<EOF
-
 use dumall;
 db.createCollection("goods");
 db.createCollection("users");
-
 EOF
 
 mongoimport --db dumall --collection goods --file /usr/local/work/dumall-goods.json
