c00725dfc71a90be6156661b87e29c56e824c1d4
Heyff12 <645068564@qq.com>
2020-05-19 15:41:37 +0800

    fix admin

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,8 +3,6 @@
 # use admin;
 # db.createUser({ user: 'root', pwd: 'rootyaya12', roles: [ { role: "userAdminAnyDatabase", db: "admin" } ] });
 mongo <<EOF
-use admin;
-db.createUser({ user: 'root', pwd: 'rootyaya12', roles: [ { role: "userAdminAnyDatabase", db: "admin" } ] });
 use dumall;
 db.createCollection("goods");
 db.createCollection("users");
